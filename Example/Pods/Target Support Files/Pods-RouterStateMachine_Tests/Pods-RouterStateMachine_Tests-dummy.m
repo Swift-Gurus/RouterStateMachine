4f5aaf76ac2b0aef811c369086378f8e8d88b8af
Alex Hmelevski <alexei.hmelevski@gmail.com>
2020-05-18 11:58:16 -0400

    moving to swift package

--- Example/Pods/Target Support Files/Pods-RouterStateMachine_Tests/Pods-RouterStateMachine_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RouterStateMachine_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_RouterStateMachine_Tests
-@end
--- Example/Pods/Target Support Files/RouterStateMachine/RouterStateMachine-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RouterStateMachine : NSObject
-@end
-@implementation PodsDummy_RouterStateMachine
-@end
